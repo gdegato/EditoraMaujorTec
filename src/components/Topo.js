62997f15e8de8e42dc509420ff6a0c59164aa3eb
gdegato <rosele.tst@gmail.com>
2022-12-22 21:08:44 -0300

    Primeiro commit

---
+++ src/components/Topo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from './Logo.js'
+import Navegação from "./Navegacao.js";
+
+const Topo = () => {
+    return (
+        <header className="topo">
+            < Logo />
+            < Navegação />
+        </header>
+    );
+};
+
+export default Topo;
